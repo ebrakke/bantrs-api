c36232b3481b0a5d94d670c13aed41b899fdcbdb
Tyler Waltze <tylerwaltze@gmail.com>
2015-09-21 19:06:04 -0400

    Small test change.
    Doing this to see how “eb deploy” handles uncommitted vs committed
    changes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ var envelope = require('../middlewares/envelope');
 var index = express.Router();
 
 index.get('/', function(req, res, next) {
-    res.locals.data = {msg: 'Hello world!'};
+    res.locals.data = {msg: 'Hello world.'};
     next();
 }, envelope);
 
